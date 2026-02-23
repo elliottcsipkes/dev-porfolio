e0fe9e40bfeee72ab58102790ec6c64cb9ae7995
Elliott Csipkes <91578219+elliottcsipkes@users.noreply.github.com>
2026-02-23 09:24:01 -0600

    Refactor App.js and App.css for improved styling and layout of the coming soon page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
-import Header from "./component/Head/Header";
-import Home from "./component/Hero/Home";
-import Portfolio from "./component/Portfolio/Portfolio";
-import Resume from "./component/Resume/Resume";
-import Contact from "./component/Contact/Contact";
-import Footer from "./component/Footer";
+// import Header from "./component/Head/Header";
+// import Home from "./component/Hero/Home";
+// import Portfolio from "./component/Portfolio/Portfolio";
+// import Resume from "./component/Resume/Resume";
+// import Contact from "./component/Contact/Contact";
+// import Footer from "./component/Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Home />
-      <Portfolio />
-      <Resume />
-      <Contact />
-      <Footer />
+      {/* <Header /> */}
+      {/* <Home /> */}
+      {/* <Portfolio /> */}
+      {/* <Resume /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+
+      <div className="coming-soon-container">
+        <h1 className="coming-soon-title">Coming Soon</h1>
+      </div>
     </>
   );
 };
